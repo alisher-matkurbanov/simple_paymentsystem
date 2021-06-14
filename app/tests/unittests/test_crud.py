23bc2465d571c6d19ee6387f8bdb91ed68a09ee0
Alisher Matkurbanov <alishermatkurbanov0408@gmail.com>
2021-06-14 13:00:33 +0500

    add get account db test

--- app/tests/unittests/test_crud.py
+++ app/tests/unittests/test_crud.py
@@ -1,6 +1,10 @@
+import datetime
+import decimal
+
 from app.database import db
-from app.schemas import AccountCreateIn, Currency
-from app.crud import uuid, create_account_with_wallet
+from app.schemas import AccountCreateIn, Currency, ExtendedAccountOut
+from app.crud import uuid
+from app import crud
 import pytest
 
 
@@ -15,7 +19,7 @@ async def test_create_account_with_wallet(monkeypatch):
         return test_uuid
     
     monkeypatch.setattr(uuid, "uuid4", mock_uuid4)
-    await create_account_with_wallet(account)
+    await crud.create_account_with_wallet(account)
     
     # check account in database
     select_account_with_wallet = (
@@ -37,12 +41,44 @@ async def test_create_account_with_wallet(monkeypatch):
     assert row["amount"] == 0
 
 
-def test_get_account_with_wallet():
-    pass
-
-
-def test_get_wallet():
-    pass
+@pytest.mark.asyncio
+async def test_get_account_with_wallet():
+    await db.connect()
+    account_uuid = uuid.uuid4()
+    wallet_uuid = uuid.uuid4()
+    name = "testname"
+    amount = decimal.Decimal("9999999.12")
+    currency = Currency.USD
+    created_at = datetime.datetime.now(tz=datetime.timezone.utc)
+    
+    expected = ExtendedAccountOut(
+        account_id=account_uuid,
+        name=name,
+        wallet_id=wallet_uuid,
+        currency=currency,
+        amount=amount,
+        created_at=created_at,
+    )
+    insert_account = "INSERT INTO account(id, name, created_at) " \
+                     "VALUES (:account_id, :name, :created_at);"
+    insert_wallet = "INSERT INTO wallet(id, account_id, amount, currency) " \
+                    "VALUES (:wallet_id, :account_id, :amount, :currency);"
+    values = {"account_id": account_uuid, "name": name, "created_at": created_at}
+    await db.execute(insert_account, values)
+    values = {
+        "account_id": account_uuid,
+        "wallet_id": wallet_uuid,
+        "amount": amount,
+        "currency": currency.value
+    }
+    await db.execute(insert_wallet, values)
+    
+    account = await crud.get_account_with_wallet(account_uuid)
+    
+    # check account in database
+    
+    await db.disconnect()
+    assert account == expected
 
 
 def test_log_transfer_transaction():
